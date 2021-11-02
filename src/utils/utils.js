7c29ac98a97f421cb69fdc575c3dbd7f8c968ef4
atharv2 <atharv@better.club>
2021-11-02 19:35:48 +0530

    Tuner with Bars

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,4 +1,3 @@
-import barObject from "../components/barsObject";
 import { stringObject } from "../components/strings";
 
 const tunerChart = [82.41, 110, 146.83, 196, 246.94, 329.63];
@@ -6,63 +5,6 @@ const tunerChart = [82.41, 110, 146.83, 196, 246.94, 329.63];
 const cents = (f1, f2) => 1200 * Math.log2(f2 / f1);
 
 const notes = ["E", "A", "D", "G", "B", "E1"];
-
-const barStructure = [
-  {
-    cent: 7.5,
-    id: [8, 9],
-    colorID: "#E58A8A",
-    color: "D7D3D3;",
-  },
-  {
-    cent: 12,
-    id: [7, 8, 9],
-    colorID: "#E58A8A",
-    color: "D7D3D3;",
-  },
-  {
-    cent: 18,
-    id: [6, 7, 8, 9],
-    colorID: "#E58A8A",
-    color: "D7D3D3;",
-  },
-  {
-    cent: 28,
-    id: [5, 6, 7, 8, 9],
-    colorID: "#E58A8A",
-    color: "D7D3D3;",
-  },
-  {
-    cent: 43,
-    id: [4, 5, 6, 7, 8, 9],
-    colorID: "#E58A8A",
-    color: "D7D3D3;",
-  },
-  {
-    cent: 67,
-    id: [3, 4, 5, 6, 7, 8, 9],
-    colorID: "#E58A8A",
-    color: "D7D3D3;",
-  },
-  {
-    cent: 104,
-    id: [2, 3, 4, 5, 6, 7, 8, 9],
-    colorID: "#E58A8A",
-    color: "D7D3D3;",
-  },
-  {
-    cent: 161,
-    id: [1, 2, 3, 4, 5, 6, 7, 8, 9],
-    colorID: "#E58A8A",
-    color: "D7D3D3;",
-  },
-  {
-    cent: 247,
-    id: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
-    colorID: "#E58A8A",
-    color: "D7D3D3;",
-  },
-];
 
 export const setValues = (pitch, clarity) => {
   const closest = tunerChart.reduce((prev, curr) =>
@@ -75,9 +17,9 @@ export const setValues = (pitch, clarity) => {
   let stringReturn = [];
   const note = notes[noteIndex];
 
-  if (clarity > 0.98 && pitch > 50) {
+  if (clarity > 0.95 && pitch > 50) {
     stringObject.map((item) => {
-      if (item.id == note) {
+      if (item.id === note) {
         stringReturn.push({
           id: note,
           text: note[0],
@@ -85,11 +27,12 @@ export const setValues = (pitch, clarity) => {
           className2: "activeStrings",
         });
       } else stringReturn.push(item);
+      return null;
     });
     if (Math.abs(centVal) < 5) {
       textReturn = {
         instruction: "Congrats 🎉",
-        text: "You tuned the guitar!",
+        text: "String tuned!",
         className: "onAudioRight",
         cents: centVal,
       };
@@ -109,8 +52,5 @@ export const setValues = (pitch, clarity) => {
       };
     }
   }
-  //   barStructure.map((item)=>{
-
-  //   })
   return { text: textReturn, string: stringReturn, cents: centVal };
 };
